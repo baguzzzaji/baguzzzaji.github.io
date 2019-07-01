699ccd3419a8e6e2eb8ab26fc8c261da219206b1
Bagus Aji Santoso <baguzzzaji@gmail.com>
2019-07-01 13:45:45 +0700

    Menyalin isi folder build

---
+++ precache-manifest.b9217910de4e6e805d9e4ce034eaef0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0101e315ad84cd76412c",
+    "url": "/static/js/main.0101e315.chunk.js"
+  },
+  {
+    "revision": "5938a2c1e0142cdb2b96",
+    "url": "/static/js/1.5938a2c1.chunk.js"
+  },
+  {
+    "revision": "0101e315ad84cd76412c",
+    "url": "/static/css/main.d7dd443d.chunk.css"
+  },
+  {
+    "revision": "8a3f47ed4c7c882e502e8c62b131febe",
+    "url": "/index.html"
+  }
+];
